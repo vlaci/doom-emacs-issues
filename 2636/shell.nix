07dc5e5b4b47a15e29775ae4c0c920ccc5876418
László Vaskó <laszlo.vasko@outlook.com>
2020-02-29 18:53:46 +0100

    Repro environment adjusted to contain pinned lsp-mode package
    https://github.com/hlissner/doom-emacs/issues/2636#issuecomment-592970983

--- 2636/shell.nix
+++ 2636/shell.nix
@@ -4,11 +4,24 @@ let
   good-ref = "7cd9e55c8cc816ddb1256d40f590840e5d6e9bbf";
   bad-ref = "c311d798ce9ca64404926ad4d5be25edc35ded60";
 
+  overrides = self: super: {
+    lsp-mode = super.lsp-mode.overrideAttrs (
+      old: {
+        src = builtins.fetchTarball "https://github.com/emacs-lsp/lsp-mode/archive/fc812bea1f825e96190b84902352b34255837f67.tar.gz";
+      }
+    );
+  };
+
+  emacsOverlay = import (builtins.fetchTarball "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz") pkgs pkgs;
+  emacsPackages = emacsOverlay.emacsPackagesFor pkgs.emacs;
+
   nix-doom-emacs = ref: pkgs.callPackage (
     builtins.fetchTarball {
       url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
     }
   ) {
+    bundledPackages = false;
+    emacsPackages = (emacsPackages.overrideScope' overrides);
     dependencyOverrides = {
       doom-emacs = builtins.fetchTarball "https://github.com/hlissner/doom-emacs/archive/${ref}.tar.gz";
     };
